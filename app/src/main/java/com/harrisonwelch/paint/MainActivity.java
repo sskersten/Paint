aac446a42800eb6a5bb640f67f1f70d7cd618bdb
Suzy Kersten <suzykersten72@outlook.com>
2018-04-16 22:52:42 -0500

    Added comments for stickers feature and frame feature

--- app/src/main/java/com/harrisonwelch/paint/MainActivity.java
+++ app/src/main/java/com/harrisonwelch/paint/MainActivity.java
@@ -1,3 +1,43 @@
+/*
+Feature: Stickers
+Description: Place stickers of stars and leaves and other zany things on your pictures!
+Variables used: AlertDialog stickerAlert;
+                int STICKER_STAR = 1;
+                int STICKER_LEAF = 2;
+                int STICKER_LEE = 3;
+                Bitmap currentBitmap;
+                Bitmap stickerStar;
+                Bitmap stickerLee;
+                Bitmap stickerLeaf;
+
+Classes used:   class Sticker
+Methods used:   setupStickerDialog(), DrawActivity
+                setupStickerBitmaps(), PictDraw
+                setSticker, PictDraw
+
+
+Feature: Frame around your picture
+Description: Put a frame around your picture to give it that elegant touch!
+Variables used:  Bitmap frame_outside;
+                 private boolean doDrawFrame = false;
+Classes used:   None
+Methods used:   toggleDoDrawFrame(), PictDraw
+                drawFrame(), PictDraw
+
+
+Feature:
+Description:
+Variables used:
+Classes used:
+Methods used:
+
+Feature:
+Description:
+Variables used:
+Classes used:
+Methods used:
+ */
+
 package com.harrisonwelch.paint;
 
 import android.app.Activity;
